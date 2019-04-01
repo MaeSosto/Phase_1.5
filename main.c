b582dfcccb571699137c412949303c9615fd1d29
MartiSosto <44461808+MartiSosto@users.noreply.github.com>
2019-04-01 12:05:30 +0200

    Inizio creazione file main

--- main.c
+++ main.c
@@ -1,4 +1,43 @@
+#include "p15test_rikaya_v0.h"
 
-int main	( 	int 	  argc,
-char * 	  argv[]
-) 	
+
+void main	(int argc, char * argv[]){
+
+
+    /*POPOLAZIONE DELLE NEW AREAS*/
+    /*Inizializzare il PC all’indirizzo dell’handler del nucleo che gestisce quell’eccezione.*/
+    
+    /*Inizializzare $SP a RAMPTOP*/
+
+    /*Inizializzare il registro di status:
+        - mascherare interrupt
+        - disabilitare virtual memory
+        - settare kernel mode ON
+        - abilitare un timer 
+    */
+
+    /*Inizializzare strutture dati di Phase1*/
+    initPcbs();
+
+    /*Inizializzare variabili del kernel:*/
+    LIST_HEAD(ready_queue);
+
+    /*Instanziare il PCB e lo stato dei 3 processi di test
+        - Interrupt abilitati
+        - Virtual Memory OFF
+        - Processor Local Timer abilitato
+        - Kernel-Mode ON
+        - $SP=RAMTOP-FRAMESIZE*n
+        - priorita’ = n
+        - Settare PC all’entry-point dei test pstate.pc_epc=(memaddr) testn
+    */
+
+    /*Inseririre i processi nella Ready Queue*/
+
+
+
+
+
+
+
+}
